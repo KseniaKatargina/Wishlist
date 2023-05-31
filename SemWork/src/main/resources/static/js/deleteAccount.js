be71cfd9c473072c9b6c42eb9ae37b951468eed1
KseniaKatargina <90481483+KseniaKatargina@users.noreply.github.com>
2023-06-01 01:14:43 +0300

    Update deleteAccount.js

--- SemWork/src/main/resources/static/js/deleteAccount.js
+++ SemWork/src/main/resources/static/js/deleteAccount.js
@@ -1,6 +1,6 @@
 const deleteBTN = document.querySelector('.delete');
 deleteBTN.addEventListener('click', ()=> {
     if(confirm('Are you sure you want to leave the site?')) {
-        window.location.href = 'http://localhost:8081/logout';
+        window.location.href = 'https://wishlist-production-edbf.up.railway.app/logout';
     }
 })
